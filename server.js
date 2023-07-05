69c1c6adccd74c96e0f09dc2de0dd7f886849899
nihara <nbmayurawasala@gmail.com>
2023-07-05 19:59:43 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+app.use(cors());
+app.use(express.json());
+const path = require("path");
+
+app.use("/", express.static(path.join(__dirname, "/build")));
+
+app.use("/*", express.static(path.join(__dirname, "/build")));
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is Running at port " + PORT);
+});
